a754850a07d93c14facbc64588730cde3017a41a
RuttoPossu <ansku.savi@gmail.com>
2025-02-10 00:48:04 +0200

    3.3

--- src/main/java/hh/backend/bookstore/bookstore/web/BookController.java
+++ src/main/java/hh/backend/bookstore/bookstore/web/BookController.java
@@ -7,13 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import hh.backend.bookstore.bookstore.domain.Book;
 import hh.backend.bookstore.bookstore.domain.BookRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -28,6 +32,23 @@ public class BookController {
         return "booklist"; 
     }
 
+    @GetMapping("/addbook")
+    public String showAddBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    @PostMapping("/addbook")
+    public String addBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+    
     @GetMapping("/index")
     public String showIndex(Model model) {
         model.addAttribute("message", "Welcome to the Bookstore!");
